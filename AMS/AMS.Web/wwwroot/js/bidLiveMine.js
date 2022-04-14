cbafc5b46b6982b705bfefa8eb83ff22748e5652
Bogdan Angelov <bgd.angelov@gmail.com>
2022-04-14 15:08:50 +0300

    Add live bids to my listings page

---
+++ AMS/AMS.Web/wwwroot/js/bidLiveMine.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+$(document).ready(function () {
+
+    var elements = document.getElementById('myListings').children;
+
+    var connection = new signalR.HubConnectionBuilder().withUrl('/bidHub').build();
+
+    connection.on('onBid', function (amount, user, listingId) {
+
+        for (let e of elements) {
+
+            if (listingId == e.dataset.id) {
+                e.children[0].children[1].children[6].textContent = amount;
+            }
+        }
+    });
+
+    connection.start();
+});
