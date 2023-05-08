7bbfc59b91790d0f9f87b0a3c1c8894dbb02ce7e
bangelov01 <bgd.angelov@gmail.com>
2023-05-08 20:58:03 +0300

    Add unit tests to AddressService

---
+++ AMS/AMS.Tests/Tests/Services/AddressServiceTests/Add.cs
@@ -0,0 +1,54 @@
+﻿namespace AMS.Tests.Tests.Services.AddressServiceTests
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using AMS.Data;
+    using AMS.Services;
+    using AMS.Data.Models;
+
+    using Xunit;
+
+    using static AMS.Tests.Database.DatabaseInitialize;
+
+    public class Add : IDisposable
+    {
+        private readonly AMSDbContext data;
+        private readonly AddressService addressService;
+
+        public Add()
+        {
+            this.data = Initialize();
+            this.addressService = new AddressService(data);
+        }
+
+        public void Dispose()
+        {
+            this.data.Dispose();
+        }
+
+        [Fact]
+        public async Task When()
+        {
+            // Arrange
+            string expectedCountry = "awesomeCountry";
+            string expectedCity = "awesomeCity";
+            string expectedAddressText = "awesomeAddressText";
+
+            // Act
+            string addressId = await addressService.Add(expectedCountry, expectedCity, expectedAddressText);
+
+            // Assert
+            Assert.NotNull(addressId);
+
+            Address? result = await data.Addresses.FindAsync(addressId);
+
+            Assert.NotNull(result);
+
+            Assert.Equal(result.Id, addressId);
+            Assert.Equal(result.Country, expectedCountry);
+            Assert.Equal(result.City, expectedCity);
+            Assert.Equal(result.AddressText, expectedAddressText);
+        }
+    }
+}
---
+++ AMS/AMS.Tests/Tests/Services/AddressServiceTests/GetId.cs
@@ -0,0 +1,67 @@
+﻿namespace AMS.Tests.Tests.Services.AddressServiceTests
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using AMS.Data;
+    using AMS.Data.Models;
+    using AMS.Services;
+
+    using Xunit;
+
+    using static AMS.Tests.Database.DatabaseInitialize;
+
+    public class GetId : IDisposable
+    {
+        private readonly AMSDbContext data;
+        private readonly AddressService addressService;
+
+        public GetId()
+        {
+            this.data = Initialize();
+            this.addressService = new AddressService(data);
+        }
+
+        public void Dispose()
+        {
+            this.data.Dispose();
+        }
+
+        [Theory]
+        [InlineData("invalid", "invalid", "invalid")]
+        [InlineData("TestCountry", "invalid", "invalid")]
+        [InlineData("TestCountry", "TestCity", "invalid")]
+        public async Task When_Parameters_Are_Invalid(string country, string city, string addressText)
+        {
+            // Act
+            string addressId = await addressService.GetId(country, city, addressText);
+
+            // Assert
+            Assert.Null(addressId);
+        }
+
+        [Fact]
+        public async Task When_AllParametersAreValid()
+        {
+            // Arrange
+            string expectedCountry = "TestCountry";
+            string expectedCity = "TestCity";
+            string expectedAddressText = "TestStreet";
+
+            // Act
+            string addressId = await addressService.GetId(expectedCountry, expectedCity, expectedAddressText);
+
+            // Assert
+            Assert.NotNull(addressId);
+
+            Address? result = await data.Addresses.FindAsync(addressId);
+
+            Assert.NotNull(result);
+
+            Assert.Equal(result.Id, addressId);
+            Assert.Equal(result.Country, expectedCountry);
+            Assert.Equal(result.City, expectedCity);
+            Assert.Equal(result.AddressText, expectedAddressText);
+        }
+    }
+}
